61dceaf414dafc19b8323d3b48a3139f2ffdea7d
Andrey Gudin <aeon150791@gmail.com>
2023-10-21 18:28:43 +0400

    feat: add phone checking response type

---
+++ src/lib/types/PhoneCheckingResponse.ts
@@ -0,0 +1,12 @@
+export interface PhoneCheckingResponse {
+  valid: boolean;
+  number: string;
+  local_format: "9090643645";
+  international_format: string;
+  country_prefix: string;
+  country_code: string;
+  country_name: string;
+  location: string;
+  carrier: string;
+  line_type: string;
+}
